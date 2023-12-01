7f534f2c8a343be12e730e8407a712c3c47b0359
phoughton <pete@investigatingsoftware.co.uk>
2023-12-01 17:09:34 +0000

    get the link and text seperatly

--- get_visible_text.py
+++ get_visible_text.py
@@ -6,8 +6,14 @@ url = "https://www.maryleboneschool.org/parent-newsletter/293.html"
 requests.packages.urllib3.disable_warnings()
 response = requests.get(url, verify=False)
 index_page = BeautifulSoup(response.text, "html.parser")
-for link in index_page.find_all('a'):
-    print("Link:", link.get('href'))
-    print("Text:", link.get_text())
-    
+# for link in index_page.find_all('a'):
+#     print("Link:", link.get('href'))
+#     print("Text:", link.get_text())
+
+print()
+
+for dated_div in index_page.find_all('div', {'class': 'jp-details'}):
+    print(dated_div.get_text())
+    print(dated_div.find('a').get('href'))
+    print()
 
