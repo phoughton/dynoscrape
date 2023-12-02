6f4842f182cee361d9efa91b2509ae4e8961b21c
Peter Houghton <pete@investigatingsoftware.co.uk>
2023-12-02 21:08:41 +0000

    build link for sub pages, not working

--- get_visible_text.py
+++ get_visible_text.py
@@ -1,13 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.maryleboneschool.org/parent-newsletter/293.html"
+url_base = "https://www.maryleboneschool.org"
+url_index_path = "/parent-newsletter/293.html"
+
+all_data = {}
 
 requests.packages.urllib3.disable_warnings()
-response = requests.get(url, verify=False)
+response = requests.get(url_base + url_index_path, verify=False)
 index_page = BeautifulSoup(response.text, "html.parser")
-for link in index_page.find_all('a'):
-    print("Link:", link.get('href'))
-    print("Text:", link.get_text())
-    
+for nl_link_a in index_page.find_all('a'):
+    nl_link = nl_link_a.get('href')
+    all_data[nl_link] = {}
+    all_data[nl_link]["link_text"] = nl_link_a.get_text()
+    print(url_base + nl_link_a)
 
+    # nl_response = requests.get(url_base + nl_link, verify=False)
+    # all_data[nl_link]["content"] = BeautifulSoup(nl_response.text, "html.parser")
+
+print(all_data)
